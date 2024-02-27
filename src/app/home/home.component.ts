a0270d274b45192fc4e6cb15e9714750fe4386d6
NamPham <phamhoangnam04032001@gmail.com>
2024-02-27 12:00:36 +0700

    ngForApDung

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,8 +9,14 @@ export class HomeComponent implements OnInit{
   public name = 'Nam Pham';
   public age = 23;
   public traiCay =['Táo','Nho','Cam','Quýt','Mít','Dừa','Ổi'];
+  public traiCay2=[
+    {ten: 'Táo', gia: 12, haGia: true},
+    {ten: 'Nho', gia: -3, haGia: false},
+    {ten: 'Cam', gia: 34, haGia: false},
+    {ten: 'Quýt', gia: 32, haGia: true},
+  ];
 
   public ngOnInit(): void {
-    console.log('trai cay = ', this.traiCay);
+    console.log('trai cay = ', this.traiCay2);
   }
 }
