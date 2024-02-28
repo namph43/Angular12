39189c02a3df45bd7a0ade99e238be44afd7f1c8
NamPham <phamhoangnam04032001@gmail.com>
2024-02-28 17:23:01 +0700

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { OnSalePipe } from './on-sale.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { OnSalePipe } from './on-sale.pipe';
     AppComponent,
     HomeComponent,
     AboutComponent,
-    OnSalePipe
+    OnSalePipe,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
